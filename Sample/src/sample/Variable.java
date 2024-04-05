c139f57f315279e746a602e0d723fbbf4eb7f8d6
dir-FY24yasudayuzuha <yuzuha.yasuda.f566@dirbato.co.jp>
2024-04-05 15:04:57 +0900

    算術演算子の練習です。

--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -6,8 +6,12 @@ public class Variable {
         var number1 = 1;
         System.out.println(number1);
         
+        /*
         String str1;
         str1 = "abc";
+        System.out.println(str1);*/
+        
+        String str1 = "abc";
         System.out.println(str1);
         
 
